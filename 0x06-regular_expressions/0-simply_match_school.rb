593352bce4a6abbd5b55f5ca2a01b0b15a94fad6
mwaniki-david <dmwaniki353@gmail.com>
2023-02-01 00:28:49 +0300

    regexp

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,11 +1,3 @@
 #!/usr/bin/env ruby
-
-# Accept one argument
-#input_string = ARGV[0]
-
-# Define the pattern to match
-pattern = /Holberton+/
-# Use scan method to get all occurrences of the pattern in the input string
-#matches = input_string.scan(pattern)
-#puts matches.empty? ? "No match found" : "Matches found: #{matches.join(', ')}"
+puts ARGV[0].scan(/School/).join
 
